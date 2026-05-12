a2e8b9b6e3bc0bbab2b55b69aeaf03ee81fd5a3a
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-05-12 14:41:51 -0700

    feat: prepend(value) and size() completed

--- LinkedList.js
+++ LinkedList.js
@@ -17,6 +17,30 @@ class LinkedList {
       this.tail = newNode;
     }
   }
+
+  prepend(value) {
+    const newNode = new Node(value);
+
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.nextNode = this.head;
+      this.head = newNode;
+    }
+  }
+
+  size() {
+    let count = 0;
+    let temp = this.head;
+
+    while (temp !== null) {
+      ++count;
+      temp = temp.nextNode;
+    }
+
+    return count;
+  }
 }
 
 const linkedList = new LinkedList();
@@ -25,6 +49,9 @@ linkedList.append(10);
 linkedList.append(11);
 linkedList.append(12);
 linkedList.append(13);
+linkedList.prepend(40);
+linkedList.prepend(50);
+console.log("The total elements in the list is: ", linkedList.size());
 
 console.log("The head is: ", linkedList.head);
 console.log("The tail is: ", linkedList.tail);
