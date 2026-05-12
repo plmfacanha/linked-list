086e1274d912af8d1f8ec5c22451b6b6ae232911
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-05-12 15:27:14 -0700

    feat: finish first part of the exercise completing toString(), findIndex(value) and more

--- LinkedList.js
+++ LinkedList.js
@@ -1,9 +1,9 @@
 import Node from "./Node.js";
 
 class LinkedList {
-  constructor(head = null, tail = null) {
-    this._head = head;
-    this._tail = tail;
+  constructor() {
+    this._head = null;
+    this._tail = null;
   }
 
   append(value) {
@@ -71,6 +71,66 @@ class LinkedList {
 
     return;
   }
+
+  pop() {
+    if (this._head === null) {
+      return;
+    }
+
+    let popValue = this._head.value;
+    this._head = this._head.nextNode;
+
+    if (this._head === null) {
+      this._tail = null;
+    }
+
+    return popValue;
+  }
+
+  contains(value) {
+    let temp = this._head;
+
+    while (temp !== null) {
+      if (temp.value === value) return true;
+
+      temp = temp.nextNode;
+    }
+
+    return false;
+  }
+
+  findIndex(value) {
+    let i = 0;
+    let temp = this._head;
+
+    while (temp !== null) {
+      if (temp.value === value) return i;
+
+      ++i;
+      temp = temp.nextNode;
+    }
+
+    return -1;
+  }
+
+  toString() {
+    let str = "";
+    let temp = this._head;
+
+    if (temp === null) {
+      return "";
+    }
+
+    while (temp !== null) {
+      str += `(${temp.value}) -> `;
+
+      temp = temp.nextNode;
+    }
+
+    str += " null";
+
+    return str;
+  }
 }
 
 const linkedList = new LinkedList();
@@ -79,8 +139,15 @@ linkedList.append(10);
 linkedList.append(11);
 linkedList.append(12);
 linkedList.prepend(40);
+linkedList.prepend(50);
 
-console.log("The total elements in the list is: ", linkedList.size());
-console.log("The head value is: ", linkedList.head());
-console.log("The tail value is: ", linkedList.tail());
-console.log("The node value is: ", linkedList.at(2));
+// console.log("The total elements in the list is: ", linkedList.size());
+console.log("The node value popped is: ", linkedList.pop());
+// console.log("The head value is: ", linkedList.head());
+// console.log("The tail value is: ", linkedList.tail());
+// console.log("The list contains? ", linkedList.contains(11));
+// console.log("The list contains? ", linkedList.contains(50));
+// console.log("The list contains? ", linkedList.contains(40));
+// console.log("The index for the value is: ", linkedList.findIndex(12));
+// console.log("The index for the value is: ", linkedList.findIndex(100));
+console.log("The list is: ", linkedList.toString());
